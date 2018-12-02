97a832b9a561724f3cec58e25ce92b225c7c0f87
vyrp <felipe.croata@gmail.com>
2018-12-02 01:58:28 -0800

    Bootstrap for Ch2Pt1

---
+++ Challenge2Part1/Challenge2Part1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2018
+{
+    public static class Challenge2Part1
+    {
+        public static void Solve()
+        {
+            string[] words = Console.In.ReadToEnd()
+                .Split('\n')
+                .Where(line => !string.IsNullOrWhiteSpace(line))
+                .Select(line => line.Trim())
+                .ToArray();
+
+            Console.WriteLine("TODO");
+        }
+    }
+}
