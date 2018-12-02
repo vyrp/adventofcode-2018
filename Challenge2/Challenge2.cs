b6ab59d693c903a4d32354bfe0c759e31f0c62d6
vyrp <felipe.croata@gmail.com>
2018-12-02 01:46:30 -0800

    Implement Challenge 2

--- Challenge2/Challenge2.cs
+++ Challenge2/Challenge2.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -8,7 +9,22 @@ namespace AdventOfCode2018
     {
         public static void Solve()
         {
-            Console.WriteLine("TODO");
+            int[] numbers = Console.In.ReadToEnd()
+                .Split('\n')
+                .Where(line => !string.IsNullOrWhiteSpace(line))
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+            int idx = 0;
+            var set = new HashSet<int>();
+            while (set.Add(sum))
+            {
+                sum += numbers[idx];
+                idx = (idx + 1) % numbers.Length;
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
