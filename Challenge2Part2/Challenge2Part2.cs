32187c79db61400da7baaa674e83548de634790a
vyrp <felipe.croata@gmail.com>
2018-12-02 02:37:14 -0800

    Implement Ch2Pt2

--- Challenge2Part2/Challenge2Part2.cs
+++ Challenge2Part2/Challenge2Part2.cs
@@ -15,7 +15,30 @@ namespace AdventOfCode2018
                 .Select(line => line.Trim())
                 .ToArray();
 
-            Console.WriteLine("TODO");
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+                for (int j = i + 1; j < words.Length; j++)
+                {
+                    if (Difference(words[i], words[j]) == 1)
+                    {
+                        Console.WriteLine(OnlySameLetters(words[i], words[j]));
+                    }
+                }
+            }
+        }
+
+        private static int Difference(string word1, string word2)
+        {
+            return word1.Zip(word2, (ch1, ch2) => ch1 != ch2).Count(b => b);
+        }
+
+        private static string OnlySameLetters(string word1, string word2)
+        {
+            return new string(
+                word1.Zip(word2, (ch1, ch2) => (ch1, ch2))
+                .Where(pair => pair.ch1 == pair.ch2)
+                .Select(pair => pair.ch1)
+                .ToArray());
         }
     }
 }
