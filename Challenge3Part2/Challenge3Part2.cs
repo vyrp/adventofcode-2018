f554ad0da9f639f975f734f486bf77e093d3e340
vyrp <felipe.croata@gmail.com>
2018-12-02 23:39:57 -0800

    Implement ch3pt2

---
+++ Challenge3Part2/Challenge3Part2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2018
+{
+    public static class Challenge3Part2
+    {
+        private static Regex inputRegex = new Regex(@"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)");
+
+        public static void Solve(string[] words)
+        {
+            var dict = words
+                .Select(line => inputRegex.Match(line))
+                .SelectMany(m => PointsInRectangle(
+                    int.Parse(m.Groups[1].Value),
+                    int.Parse(m.Groups[2].Value),
+                    int.Parse(m.Groups[3].Value),
+                    int.Parse(m.Groups[4].Value),
+                    int.Parse(m.Groups[5].Value)))
+                .GroupBy(t => (t.x, t.y), t => t.id)
+                .ToDictionary(g => g.Key, g => g.ToList());
+
+            var haveOverlap = dict.Values.Where(ids => ids.Count >= 2).SelectMany(ids => ids).ToHashSet();
+            Console.WriteLine(Enumerable.Range(1, words.Length).Single(id => !haveOverlap.Contains(id)));
+        }
+
+        private static IEnumerable<(int id, int x, int y)> PointsInRectangle(int id, int left, int top, int w, int h)
+        {
+            for (int i = left; i < left + w; i++)
+            {
+                for (int j = top; j < top + h; j++)
+                {
+                    yield return (id, i, j);
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -31,6 +31,9 @@ namespace AdventOfCode2018
                     case "3.1":
                         Challenge3Part1.Solve(ReadInput());
                         break;
+                    case "3.2":
+                        Challenge3Part2.Solve(ReadInput());
+                        break;
                     default:
                         Console.Error.WriteLine($"Challenge not supported: '{args[0]}'.");
                         return 1;
