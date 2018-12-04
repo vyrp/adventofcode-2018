a2ace894e8c24f74e1bdcfe85b02a955def20979
vyrp <felipe.croata@gmail.com>
2018-12-04 00:05:40 -0800

    Initial implementation of Ch4Pt1

--- Challenge4Part1/Challenge4Part1.cs
+++ Challenge4Part1/Challenge4Part1.cs
@@ -8,9 +8,46 @@ namespace AdventOfCode2018
 {
     public static class Challenge4Part1
     {
+        private static Regex fullInputRegex = new Regex(@"\[\d+-\d+-\d+ \d+:(\d+)\] (.+)");
+        private static Regex guardIdRegex = new Regex(@"Guard #(\d+) begins shift");
+
         public static void Solve(string[] lines)
         {
-            Console.WriteLine("TODO");
+            var items = lines
+                .OrderBy(line => line)
+                .Select(line => fullInputRegex.Match(line))
+                .Select(m => (minute: int.Parse(m.Groups[1].Value), text: m.Groups[2].Value));
+
+            var minutesByGuard = new Dictionary<string, int[]>();
+            string currentGuard = default;
+            int lastTime = default;
+            foreach (var item in items)
+            {
+                if (item.text == "falls asleep")
+                {
+                    lastTime = item.minute;
+                }
+                else if (item.text == "wakes up")
+                {
+                    var minutes = minutesByGuard[currentGuard];
+                    for (int m = lastTime; m < item.minute; m++)
+                    {
+                        minutes[m]++;
+                    }
+                }
+                else
+                {
+                    currentGuard = guardIdRegex.Match(item.text).Groups[1].Value;
+                    if (!minutesByGuard.ContainsKey(currentGuard))
+                    {
+                        minutesByGuard[currentGuard] = new int[60]; // Starts 0-filled.
+                    }
+                }
+            }
+
+            (string mostAsleepGuard, int[] hisMinutes) = minutesByGuard.OrderByDescending(kvp => kvp.Value.Sum()).First();
+
+            Console.WriteLine(Array.IndexOf(hisMinutes, hisMinutes.Max()) * int.Parse(mostAsleepGuard));
         }
     }
 }
