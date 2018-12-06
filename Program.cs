ecf8fab94de8737576abe56732093fbea842b7a7
vyrp <felipe.croata@gmail.com>
2018-12-05 22:49:27 -0800

    Bootstrap for Ch5Pt2

--- Program.cs
+++ Program.cs
@@ -43,6 +43,9 @@ namespace AdventOfCode2018
                     case "5.1":
                         Challenge5Part1.Solve(ReadInput());
                         break;
+                    case "5.2":
+                        Challenge5Part2.Solve(ReadInput());
+                        break;
                     default:
                         Console.Error.WriteLine($"Challenge not supported: '{args[0]}'.");
                         return 1;
