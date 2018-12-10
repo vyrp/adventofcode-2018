ee0abe418013ed0ac851c8a26ac4d2e8390cb0e9
vyrp <felipe.croata@gmail.com>
2018-12-09 20:15:46 -0800

    Implementation for Ch5Pt2

--- Challenge5Part2/Challenge5Part2.cs
+++ Challenge5Part2/Challenge5Part2.cs
@@ -12,7 +12,38 @@ namespace AdventOfCode2018
     {
         public static void Solve(IEnumerable<string> lines)
         {
-            Console.WriteLine("TODO");
+            string input = lines.First();
+
+            Console.WriteLine(
+                'a'.To('z')
+                    .Select(ch => input
+                        .Replace(ch.ToString(), "")
+                        .Replace(char.ToUpper(ch).ToString(), ""))
+                    .Select(SolveForOne)
+                    .Min());
+        }
+
+        private static int SolveForOne(string input)
+        {
+            var s = new Stack<char>();
+            foreach (var ch in input)
+            {
+                if (s.Count > 0 && Matches(ch, s.Peek()))
+                {
+                    s.Pop();
+                }
+                else
+                {
+                    s.Push(ch);
+                }
+            }
+
+            return s.Count;
+        }
+
+        private static bool Matches(char a, char b)
+        {
+            return (a ^ b) == 0b0010_0000;
         }
     }
 }
