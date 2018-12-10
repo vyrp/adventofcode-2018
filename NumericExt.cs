fee981e644fff725fa66ef18d46bd4ea8ab36503
vyrp <felipe.croata@gmail.com>
2018-12-09 20:11:39 -0800

    Add NumericExt.To(from, to)

---
+++ NumericExt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2018
+{
+    public static class NumericExt
+    {
+        public static IEnumerable<char> To(this char from, char to)
+        {
+            for (char ch = from; ch <= to; ch++)
+            {
+                yield return ch;
+            }
+        }
+
+        public static IEnumerable<int> To(this int from, int to)
+        {
+            for (int n = from; n <= to; n++)
+            {
+                yield return n;
+            }
+        }
+    }
+}
