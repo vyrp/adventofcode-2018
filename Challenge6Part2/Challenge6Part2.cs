d4c547568f86abf50b6a75dfde52619815d7b2e4
vyrp <felipe.croata@gmail.com>
2018-12-16 03:56:30 -0800

    Implementation for Ch6Pt2

--- Challenge6Part2/Challenge6Part2.cs
+++ Challenge6Part2/Challenge6Part2.cs
@@ -12,7 +12,34 @@ namespace AdventOfCode2018
     {
         public static void Solve(IEnumerable<string> lines)
         {
-            Console.WriteLine("TODO");
+            var r = new Regex(@"(\d+), (\d+)");
+
+            var points = lines
+                .Select(line => r.Match(line))
+                .Select(m => ToPoint(m.Groups[1].Value, m.Groups[2].Value))
+                .ToArray();
+
+            var minX = points.Min(p => p.x);
+            var maxX = points.Max(p => p.x);
+            var minY = points.Min(p => p.y);
+            var maxY = points.Max(p => p.y);
+
+            var sums =
+                from x in minX.To(maxX)
+                from y in minY.To(maxY)
+                select points.Select(p => Dist(x, y, p.x, p.y)).Sum();
+
+            Console.WriteLine(sums.Count(s => s < 10000));
+        }
+
+        private static int Dist(int x1, int y1, int x2, int y2)
+        {
+            return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+        }
+
+        private static (int x, int y) ToPoint(string s1, string s2)
+        {
+            return (int.Parse(s1), int.Parse(s2));
         }
     }
 }
