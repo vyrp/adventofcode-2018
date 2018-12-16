efcb1e2949e1d62c6d7e56c22b6697f2670c672d
vyrp <felipe.croata@gmail.com>
2018-12-16 05:00:44 -0800

    Implementation for Ch7Pt2

--- Challenge7Part2/Challenge7Part2.cs
+++ Challenge7Part2/Challenge7Part2.cs
@@ -5,14 +5,89 @@ using System.Linq;
 using System.Text.RegularExpressions;
 
 using MoreLinq;
+using AdventOfCode2018.Challenge7;
 
 namespace AdventOfCode2018
 {
     public static class Challenge7Part2
     {
+        private const int N = 5;
+
         public static void Solve(IEnumerable<string> lines)
         {
-            Console.WriteLine("TODO");
+            var r = new Regex(@"Step (\w) must be finished before step (\w) can begin.");
+
+            (char start, char end)[] edges = lines
+                .Select(line => r.Match(line))
+                .Select(m => ToItem(m.Groups[1].Value, m.Groups[2].Value))
+                .ToArray();
+
+            Dictionary<char, char[]> forwardDict = edges
+                .GroupBy(e => e.start, e => e.end)
+                .ToDictionary(g => g.Key, g => g.ToArray());
+
+            Dictionary<char, HashSet<char>> backwardsDict = edges
+                .GroupBy(e => e.end, e => e.start)
+                .ToDictionary(g => g.Key, g => new HashSet<char>(g));
+
+            // Start with the steps that don't have requirements.
+            var q = new SortedSet<char>(forwardDict.Keys.Except(backwardsDict.Keys));
+            var workers = 1.To(N).Select(_ => new Worker()).ToArray();
+            int iter = 0;
+            while (q.Count > 0 || workers.Any(w => w.IsWorking))
+            {
+                foreach (var w in workers)
+                {
+                    char? maybeJob = w.Tick();
+
+                    if (maybeJob.HasValue)
+                    {
+                        OnCompleted(maybeJob.Value);
+                    }
+                }
+
+                foreach (var w in workers)
+                {
+                    if (q.Count == 0)
+                    {
+                        break;
+                    }
+
+                    if (!w.IsWorking)
+                    {
+                        var step = q.First();
+                        q.Remove(step);
+                        w.WorkOn(step);
+                    }
+                }
+
+                iter++;
+            }
+
+            Console.WriteLine(iter - 1);
+
+            // Local function
+            void OnCompleted(char step)
+            {
+                if (forwardDict.TryGetValue(step, out char[] nextSteps))
+                {
+                    foreach (char end in nextSteps)
+                    {
+                        HashSet<char> stepsStillToDo = backwardsDict[end];
+                        stepsStillToDo.Remove(step);
+
+                        if (stepsStillToDo.Count == 0)
+                        {
+                            q.Add(end);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static (char x, char y) ToItem(string s1, string s2)
+        {
+            return (s1[0], s2[0]);
         }
     }
 }
---
+++ Challenge7Part2/Worker.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+using MoreLinq;
+
+namespace AdventOfCode2018.Challenge7
+{
+    internal sealed class Worker
+    {
+        private const int Offset = 61;
+
+        private char currentJob;
+
+        private int timeLeft;
+
+        public bool IsWorking => timeLeft > 0;
+
+        public char? Tick()
+        {
+            if (IsWorking)
+            {
+                timeLeft--;
+                if (timeLeft == 0)
+                {
+                    return currentJob;
+                }
+            }
+
+            return null;
+        }
+
+
+        public void WorkOn(char step)
+        {
+            currentJob = step;
+            timeLeft = Offset + step - 'A';
+        }
+    }
+}
