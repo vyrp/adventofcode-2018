e303bf1504876acd8de80538db4b7417e44dde06
vyrp <felipe.croata@gmail.com>
2018-12-16 05:50:42 -0800

    Implementation for Ch8Pt1

--- adventofcode-2018/Challenge8Part1/Challenge8Part1.cs
+++ adventofcode-2018/Challenge8Part1/Challenge8Part1.cs
@@ -12,7 +12,28 @@ namespace AdventOfCode2018
     {
         public static void Solve(IEnumerable<string> lines)
         {
-            Console.WriteLine("TODO");
+            string content = lines.Single();
+            var numbers = new Queue<int>(content.Split(' ').Select(int.Parse));
+            Console.WriteLine(Dfs(numbers));
+        }
+
+        private static int Dfs(Queue<int> toks)
+        {
+            int childCount = toks.Dequeue();
+            int metaCount = toks.Dequeue();
+
+            int sum = 0;
+            for (int i = 0; i < childCount; i++)
+            {
+                sum += Dfs(toks);
+            }
+
+            for (int i = 0; i < metaCount; i++)
+            {
+                sum += toks.Dequeue();
+            }
+
+            return sum;
         }
     }
 }
