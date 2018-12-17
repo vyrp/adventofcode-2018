e004f508350f0465247fec43a715db9b47d69486
vyrp <felipe.croata@gmail.com>
2018-12-16 17:49:30 -0800

    Implementation for Ch9Pt1

--- adventofcode-2018/Challenge9Part1/Challenge9Part1.cs
+++ adventofcode-2018/Challenge9Part1/Challenge9Part1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2018
 {
@@ -7,7 +9,41 @@ namespace AdventOfCode2018
     {
         public static void Solve(IEnumerable<string> lines)
         {
-            Console.WriteLine("TODO");
+            var r = new Regex(@"(\d+) players; last marble is worth (\d+) points");
+
+            (int players, int last) = lines
+                .Select(line => r.Match(line))
+                .Select(m => ToItem(m.Groups[1].Value, m.Groups[2].Value))
+                .Single();
+
+            var marbles = new List<int> { 0 };
+            var points = new int[players];
+
+            int current = 0;
+            for (int i = 1; i <= last; i++)
+            {
+                if (i % 23 == 0)
+                {
+                    points[i % players] += i;
+
+                    current = (current - 7 + marbles.Count) % marbles.Count;
+                    var v = marbles[current];
+                    marbles.RemoveAt(current);
+                    points[i % players] += v;
+                }
+                else
+                {
+                    current = (current + 2) % marbles.Count;
+                    marbles.Insert(current, i);
+                }
+            }
+
+            Console.WriteLine(points.Max());
+        }
+
+        private static (int x, int y) ToItem(string s1, string s2)
+        {
+            return (int.Parse(s1), int.Parse(s2));
         }
     }
 }
